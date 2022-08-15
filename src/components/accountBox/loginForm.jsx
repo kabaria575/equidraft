86e8b1b589c4b07307fd2a756de7aeeea413f7ae
Zachariarian <zachariarian@gmail.com>
2022-08-15 11:51:58 +0300

    try

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -14,19 +14,59 @@ export function LoginForm(props) {
   const { switchToSignup } = useContext(AccountContext);
   const [details, setDetails] = useState({name: "", email: "", password: ""});
 
+  const submitHandler = e => {
+    e.preventDefault();
+
+    Login(details);
+  }
+
+  const [user, setUser] = useState({name: "", email: ""})
+  const [error, setError] = useState("");
+
+  const adminUser = {
+    email: "zachariarian@gmail.com",
+    password: "123@abc"
+  }
+
     const Login = details => {
       console.log(details);
+
+      if (details.email === adminUser.email && details.password === adminUser.password) {
+        console.log("Logged in");
+        setUser({
+          name: details.name,
+          email: details.email
+        });
+      } else {
+        console.log("Details do not match!");
+        setError("Details do not match!");
+      }
+    }
+
+
+    const Logout = details => {
+      setUser({name: "", email: ""});
+      console.log(Logout);
+  
   }
   return (
     <>
-
-    
+    <form onSubmit={submitHandler}></form>
+    <Login>Login</Login>
+    {(error !== "") ? (<error>({error})</error> ) : ""}
+    <React.Fragment> {(user.email !== "") ? (
+      "Welcome", <><span>{user.name} </span>
+      <button onClick={Logout}> Logout </button></> )
+      : (
+        <LoginForm Login={Login} error={error}/>
+      )} </React.Fragment>
     <BoxContainer>
       <FormContainer>
-        <Input type="text" name="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
+        <Input type="text" placeholder="Name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
         <Input type="email" placeholder="Email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
         <Input type="password" placeholder="Password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-      </FormContainer>
+        <input type="submit" value={Login}/>
+        </FormContainer>
       <Marginer direction="vertical" margin={10} />
       <MutedLink href="#">Forget your password?</MutedLink>
       <Marginer direction="vertical" margin="1.6em" />
@@ -35,7 +75,7 @@ export function LoginForm(props) {
       <MutedLink href="#">
         Don't have an account?{" "}
         <BoldLink href="#" onClick={switchToSignup}>
-          Signup
+          Login
         </BoldLink>
       </MutedLink>
     </BoxContainer>
